4d80bbd989d01e0492efc2c4b630c4d9b972d849
Alexander Collin Kollan <finapocalypse@gmail.com>
2019-07-30 19:11:32 +0300

    All checkers completed
    All checkers completed. Refactoring of checkers to fit hand comparison checkers is needed.

--- js/check.js
+++ js/check.js
@@ -11,36 +11,105 @@ const check = {
             console.log('2 ',chared)
             let convChared = chared.map(x => x.replace(/^101|^102|^103|^104/g,"140"+x[x.length-1]))
             // let sortedConvChared = convChared.sort((a,b)=>b-a)
-            // console.log('3 ',sortedConvChared)
-            let suits = convChared.map(x => parseInt(x.slice(-1)))
+            console.log('3 ',convChared)
+            let cardSuits = convChared.map(x => parseInt(x.slice(-1)))
             
-            console.log('4 ', suits)
-            let numbers = convChared.map(x => parseInt(x.slice(0,3)))
-            console.log('5 ', numbers)
+            console.log('4 ', cardSuits)
+            let cardValues = (convChared.map(x =>  x.length ===4 ? parseInt(x.slice(0,2)) : parseInt(x.slice(0,1)))).sort((a,b)=>b-a)
+            let cardCodes = (convChared.map(Number)).sort((a,b)=>b-a)
+            
+            console.log('5 ', cardValues)
+            console.log('6 ', cardCodes)
             // return [, suits];
+            // 14..03   80....3
+            return [cardCodes, cardValues, cardSuits];
         },
     royalFlush: (h)=>{
         console.log(h)
-        let isFlush = h[1].every((c)=>{
-            return c==h[1][0]
+        let isFlush = h[2].every((c)=>{
+            return c==h[2][0]
         })
         let straightIds=[];
-        for (let i = 1; i < h[0].length; i++) {
-                straightIds.push(Math.floor(h[0][i-1]/100)-Math.floor(h[0][i]/100))
+        for (let i = 1; i < h[1].length; i++) {
+                straightIds.push(h[1][i-1]-h[1][i])
         }
         let isStraight = straightIds.every((c)=>{
             return c === 1;
         })
         let hasAce = (h[0][0]===1401)||(h[0][0]===1402)||(h[0][0]===1403)||(h[0][0]===1404)?true:false;
+        console.log(h)
+        console.log(isFlush)
+        console.log(isStraight)
         console.log(hasAce)
-        if(hasAce && isStraight && isFlush ){
+        console.log((h[1].reduce((p,c)=> p+c)))
+        if(hasAce && isStraight && isFlush && (h[1].reduce((p,c)=> p+c))===60){
             console.log('IT\'S A FLUSH ROYAL!!')
             return 10;
+        }else if(isFlush&& isStraight){
+            console.log('IT\'S A STRAIGHT FLUSH!!')
+            return 9;
+        }else if(isFlush){
+            console.log('IT\'S A FLUSH!!')
+            return 6;
+        }else if(isStraight){
+            console.log('IT\'S A STRAIGHT!!')
+            return 5;
+        }else{
+            console.log('TPT TPT TPT')
+            return 0;
         }
-        // if(isFlush&& isStraight){
-        //     console.log('IT\'S A STRAIGHT FLUSH!!')
-        //     return 9;
-        // }
+
+    },
+    getOccurrence: (h, card)=>{
+        //Makes a filtered array which contains the same cards if present.
+        //The length of this array is how many times a specific card occured.
+        return [card, h.filter((x) => (x === card)).length];
+    },
+    pairs: (h) =>{
+        let dupes = [];
+        for (let i = 0; i < h[1].length; i++) {
+            let pair = check.getOccurrence(h[1],h[1][i]) ;
+            if(pair[1]>1) dupes.push(pair)
+        }
+        let pairs = Array.from(new Set(dupes.map(JSON.stringify)), JSON.parse)
+        console.log('These are the DUPES :', dupes)
+        console.log('These are the PAIRS :', pairs)
+        return pairs;
         
+
+    },
+    onePair: (p)=>{
+        return p.length >= 1 ? [true,p[0][0]] : false;
+    },
+    twoPairs: (p)=>{
+        return p.length >= 2 ? [true, p[0][0],p[1][0]] : false;
+    },
+    threePairs:(p)=>{
+        for (let i = 0; i < p.length; i++) {
+            if(p[i][1]===3){
+                return [true, p[i][0]]
+            }
+        }
+        return false;
+    },
+    full:(p)=>{
+        for (let i = 0; i < p.length; i++) {
+            if(p[i][1]===4){
+                return [true, p[i][0]]
+            }
+        }
+        return false;
+    },
+    fullHouse:(p)=>{
+        if(p.length===2){
+            for (let i = 0; i < p.length; i++) {
+                if(p[i][1]===3){
+                    return [true, p[i][0]]
+                }
+            }
+        }
+        
+        return false;
     }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
 
-
+let checker = true;
 
 
 // console.log(value)  
@@ -12,6 +12,9 @@ function compare(){
 }
 
 $("#reroll").click(() => {
+    let times = 0;
+    checker = true;
+    // while(checker){
         $(".npc-card-container,.table-container,.player-card-container").empty();
         deck = [101,201,301,401,501,601,701,801,901,1001,1101,1201,1301,
             102,202,302,402,502,602,702,802,902,1002,1102,1202,1302,
@@ -22,15 +25,23 @@ $("#reroll").click(() => {
         playerCards = [];
         usedCards = [];
         generate.hand('a',5);
-        
-        // if(check.royalFlush(check.sort(playerCards))){
-        //     console.log('ROYAL FLUSH!!!!!!!!!!!!!!!')
-        // }  
-        if(check.royalFlush(check.sort(npcCards))){
-
-            console.log('ROYAL FLUSH!!!!!!!!!!!!!!!')
+        times++;
+        if(check.royalFlush(check.sort(playerCards))>0){
+            console.log(`Draw: ${times}`)
+            checker = false;
+            // break;
         }  
+        let pcards = check.sort(playerCards)
+        let pairingPCards = check.pairs(pcards)
+        console.log('1:', check.onePair(pairingPCards))
+        console.log('2:', check.twoPairs(pairingPCards))
+        console.log('3:', check.threePairs(pairingPCards))
+        console.log('23:', check.fullHouse(pairingPCards))
+        console.log('4:', check.full(pairingPCards))
+        // console.log(check.royalFlush(check.sort(playerCards)))
         // if(check.royalFlush(check.sort(tableCards))){
         //     console.log('ROYAL FLUSH!!!!!!!!!!!!!!!')
-        // }  
+        // } 
+    // }
+
 });
